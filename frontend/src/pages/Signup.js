cc3f4fde0c78f9a6d580345ffb80d5ccd50c3af1
Saravanan <saravananbofficial07@gmail.com>
2025-05-17 13:19:47 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return toast.error('All fields are required')
         }
         try {
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://login-form-api-blond.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
